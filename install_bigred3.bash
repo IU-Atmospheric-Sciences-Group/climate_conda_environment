310b76afc5e1e9ac4e1b0eaa374611f91934e45c
Sam Smith <45645265+sjsmith757@users.noreply.github.com>
2021-06-17 13:31:32 -0400

    Allow install script to update conda environment

--- install_bigred3.bash
+++ install_bigred3.bash
@@ -7,21 +7,33 @@ set -x
 PYTHON_VERSION=3.8
 ENVIRONMENT_NAME=climate_py${PYTHON_VERSION/./}
 
-conda create -y --quiet -c conda-forge -n ${ENVIRONMENT_NAME} python=${PYTHON_VERSION}
+conda env list > $HOME/.my_conda_envs
 
-source activate $ENVIRONMENT_NAME
+#if the current environment name is already found in your environments, just update instead
+if [["$(cat $HOME/.my_conda_envs)" != *"${ENVIRONMENT_NAME} "* ]]; then
 
-# install mpi4py
-#MPICC="mpicc -shared" pip install --no-binary=mpi4py mpi4py
+  conda create -y --quiet -c conda-forge -n ${ENVIRONMENT_NAME} python=${PYTHON_VERSION}
 
-# install mamba
-conda install -c conda-forge mamba --quiet --yes
+  source activate $ENVIRONMENT_NAME
 
-# install conda packages (channels managed in YAML file)
-mamba install --yes --file requirements.yml
+  # install mpi4py
+  #MPICC="mpicc -shared" pip install --no-binary=mpi4py mpi4py
 
-# install pip packages
-pip install -r requirements_pip.txt
+  # install mamba
+  conda install -c conda-forge mamba --quiet --yes
 
-# activate the jupyter kernel
-ipython kernel install --name "${ENVIRONMENT_NAME}" --user
+  # install conda packages (channels managed in YAML file)
+  mamba install --yes --file requirements.yml
+
+  # install pip packages
+  pip install -r requirements_pip.txt
+
+  # activate the jupyter kernel
+  ipython kernel install --name "${ENVIRONMENT_NAME}" --user
+
+else
+
+  source activate $ENVIRONMENT_NAME
+  
+  mamba env update -n ${ENVIRONMENT_NAME} python=${PYTHON_VERSION} --file requirements.yml
+
