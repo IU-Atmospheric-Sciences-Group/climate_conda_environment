5d8eb2c583d35aea0932686b53109d277e2a2e8e
Travis A. O'Brien <obrienta@iu.edu>
2021-06-17 10:28:35 -0400

    Initial commit -- successfully install environment on bigred3 as of 6/17/21

---
+++ install_bigred3.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+module load anaconda
+
+set -e
+set -x
+
+PYTHON_VERSION=3.8
+ENVIRONMENT_NAME=climate_py${PYTHON_VERSION/./}
+
+conda create -y --quiet -c conda-forge -n ${ENVIRONMENT_NAME} python=${PYTHON_VERSION}
+
+source activate $ENVIRONMENT_NAME
+
+# install mpi4py
+#MPICC="mpicc -shared" pip install --no-binary=mpi4py mpi4py
+
+# install mamba
+conda install -c conda-forge mamba --quiet --yes
+
+# install conda packages
+mamba install -c conda-forge --yes --file requirements.txt
+
+# install pip packages
+pip install -r requirements_pip.txt
+
+# activate the jupyter kernel
+ipython kernel install --name "${ENVIRONMENT_NAME}" --user
