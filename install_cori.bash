d2ed60bbcc966d196208e2c68daefcf6917791d1
Travis A. O'Brien <TAOBrien@lbl.gov>
2021-10-15 14:19:44 -0700

    Fixed install_cori.bash script so it installs a fresh environment with no intervention required; added PyNIO and PyNGL to the yml file

--- install_cori.bash
+++ install_cori.bash
@@ -6,6 +6,8 @@ set -x
 PYTHON_VERSION=3.8
 ENVIRONMENT_NAME=climate_py${PYTHON_VERSION/./}
 
+module load python
+
 #save list of conda envs as temp file
 conda env list > $HOME/.my_conda_envs
 
@@ -14,7 +16,7 @@ if [[ "$(cat $HOME/.my_conda_envs)" != *"${ENVIRONMENT_NAME} "* ]]; then
 
   conda create -y --quiet -c conda-forge -n ${ENVIRONMENT_NAME} python=${PYTHON_VERSION}
 
-  conda activate $ENVIRONMENT_NAME
+  source activate $ENVIRONMENT_NAME
 
 
   # install mamba
@@ -22,7 +24,7 @@ if [[ "$(cat $HOME/.my_conda_envs)" != *"${ENVIRONMENT_NAME} "* ]]; then
 
   # install conda packages (channels managed in YAML file)
   # pip dependencies also managed in YAML file
-  mamba env udpate -f requirements.yml
+  mamba env update -f requirements.yml
 
   # compile mpi4py using NERSC wrappers
   module swap PrgEnv-${PE_ENV,,} PrgEnv-gnu
@@ -34,7 +36,7 @@ if [[ "$(cat $HOME/.my_conda_envs)" != *"${ENVIRONMENT_NAME} "* ]]; then
 #if it is, just update the environment
 else
 
-  conda activate $ENVIRONMENT_NAME
+  source activate $ENVIRONMENT_NAME
   
   mamba env update -f requirements.yml
 
